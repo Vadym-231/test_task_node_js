8069a2ff5d25be04d4cb4a7fb316b896b17a7f5f
Vadym-231 <58479976+Vadym-231@users.noreply.github.com>
2023-10-25 01:54:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 let dotenv = require('dotenv').config();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 const { getPostByIdEndpoint, getPostsEndpoint } = require('./src/controllers/posts');
 
 
